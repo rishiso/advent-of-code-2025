ec54d5f077d4407d9246716cf50eebe90440dfa4
Rishi Soni <rishisoni.5678@gmail.com>
2025-12-05 20:44:43 -0500

    Day 2 Part 2

---
+++ day2_part2.py
@@ -0,0 +1,28 @@
+import sys
+import bisect
+
+line = sys.stdin.read().strip()
+
+intervals = []
+
+for interval in line.split(","):
+    intervals.append(tuple(int(i) for i in interval.split("-")))
+
+intervals.sort()
+
+def isInvalid(n):
+    ind = bisect.bisect_right(intervals, (n, float('inf')))
+    return ind - 1 >= 0 and n <= intervals[ind - 1][1]
+
+invalid = set()
+for i in range(1, 99999): # Intervals contain at most 10 digits
+    for j in range(2, 10):
+        if len(str(i)) * j > 10:
+            continue
+
+        combined = int(str(i) * j)
+        if isInvalid(combined):
+            invalid.add(combined)
+
+print()
+print(sum(invalid))
